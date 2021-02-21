aecb11426a4d1df88fdf52d29904f614c8d40c36
Christopher Bussick <christopher.bussick@web.de>
2021-02-21 01:04:26 +0100

    Cleaned up

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -8,9 +8,6 @@ import "styled-components";
 // and extend them!
 declare module "styled-components" {
   export interface DefaultTheme {
-    // Add more properties here like this:
-    // borderRadius: string;
-
     colors: {
       primary: string;
       secondary: string;
